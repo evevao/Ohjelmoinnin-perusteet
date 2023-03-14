8f3d328a2c55221c5fe2ee731259210e9426cccf
Viola Ollila <AD7939@student.jamk.com>
2023-03-14 11:32:45 +0200

    LAB02_04

---
+++ LAB02/LAB02_04.py
@@ -0,0 +1,14 @@
+#LAB02 tehtävä 04
+import datetime
+
+#Teksti komennot
+
+fname = input("Anna etunimesi: ")
+ika = int((input("Syntymävuotesi: ")))
+ika2 = datetime.datetime.today()
+ika3 = int(ika2.year)
+ika4 = (ika3 - ika)
+
+
+#Printti komennot
+print("Hei", fname, ", olet", ika4 , "vuotta.")
