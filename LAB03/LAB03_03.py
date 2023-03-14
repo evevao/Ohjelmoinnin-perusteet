6c4b7ad5a98e1e8cee2b41ac551ba61005c7024c
Viola Ollila <AD7939@student.jamk.com>
2023-03-14 13:43:46 +0200

    LAB03_03

--- LAB03/LAB03_03.py
+++ LAB03/LAB03_03.py
@@ -1,3 +1,17 @@
 #LAB03 tehtävä 03
 
 #Teksti komennot
+num1 = int(input("Anna kokonaisluku: "))
+
+
+#printti komennot
+if num1 == 10: 
+    print("Luku on 10 tai 20")
+elif num1 == 20: 
+    print("Luku on 10 tai 20")
+elif num1 == 100:
+    print("Luku on 100 tai 200")
+elif num1 == 200:
+    print("Luku on 100 tai 200")
+else: 
+    print("Luku on: ", num1)
