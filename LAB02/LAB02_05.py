94668d477c8509e3c5ea64d3c3d527a6030abeb6
Viola Ollila <AD7939@student.jamk.com>
2023-03-14 12:06:06 +0200

    LAB02_05

---
+++ LAB02/LAB02_05.py
@@ -0,0 +1,10 @@
+#LAB02 tehtävä 05
+
+#Teksti komennot
+fname = input("Anna etunimesi: ")
+eka = fname[0]
+letter = len(fname)
+
+#printti komennot
+print("Nimessäsi", fname, "on", letter, "kirjainta.") 
+print(eka * letter)
