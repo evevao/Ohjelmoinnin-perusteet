f983b035945baa9ff919b7460afaab719db46c4c
Viola Ollila <AD7939@student.jamk.com>
2023-03-14 10:01:24 +0200

    LAB02_03

---
+++ LAB02/LAB02_03.py
@@ -0,0 +1,9 @@
+#LAB02 tehtävä 03
+
+#Teksti komennot
+nimi = input("Anna nimesi: ")
+Etunimi, *Sukunimi = nimi.split()
+
+#printti komennot?
+print("Etunimi: = {Etunimi}".format (Etunimi=Etunimi))
+print("Sukunimi: = {Sukunimi}".format (Sukunimi=" ".join (Sukunimi)))
