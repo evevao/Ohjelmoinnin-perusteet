e46a0307ff99586ffeaf2664c49c227dd47637d5
Viola Ollila <AD7939@student.jamk.com>
2023-03-17 23:06:02 +0200

    väli tallennus

---
+++ LAB05/LAB05_01_.py
@@ -0,0 +1,5 @@
+#LAB05 tehtävä 01
+
+def show_info():
+    return "Olen info-funktio"
+print(show_info())
---
+++ LAB05/LAB05_03.apua.py
@@ -0,0 +1,11 @@
+#LAB05 tehtävä 03
+
+text = "Anna alla kolme lukua."
+print(text)
+num1 = float(input("Anna luku yksi: "))
+num2 = float(input("Anna luku kaksi: "))
+num3 = float(input("Anna luku kolme: "))
+
+def average():
+    return print((num1+num2+num3)/3)
+print(average())
