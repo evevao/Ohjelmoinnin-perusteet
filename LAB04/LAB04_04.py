d604f9e55289b4fc9d37173456b4b0ce4cc33473
Viola Ollila <AD7939@student.jamk.com>
2023-03-17 12:37:55 +0200

    korjaus

--- LAB04/LAB04_04.py
+++ LAB04/LAB04_04.py
@@ -5,6 +5,6 @@ luku = int(input("Anna numero väliltä 1-10: "))
 rivi = int(1)
 
 #printti komennot
-for i in range(1, int(luku)):
+for i in range(1, int(luku) + 1):
     print("luvun",rivi, "neliö on", i ** 2)
     rivi = rivi + int(1)
