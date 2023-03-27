337260a45d32ab1ebbb201a083bb52c39b2154f5
Viola Ollila <AD7939@student.jamk.com>
2023-03-27 10:46:43 +0300

    LAB07_01

---
+++ LAB07/LAB07_01.py
@@ -0,0 +1,31 @@
+#LAB07 tehtävä 01
+
+class Pelikorrti:
+    maa = ""
+    arvo = 0
+
+kortti1 = Pelikorrti()
+kortti1.maa = "hertta"
+kortti1.arvo = 14
+
+kortti2 = Pelikorrti()
+kortti2.maa = "risti"
+kortti2.arvo = 7
+
+kortti3 = Pelikorrti()
+kortti3.maa = "pata"
+kortti3.arvo = 3
+
+kortti4 = Pelikorrti()
+kortti4.maa = "ruutu"
+kortti4.arvo = 10
+
+kortti5 = Pelikorrti()
+kortti5.maa = "pata"
+kortti5.arvo = 2
+
+print("1. kortti on:", kortti1.maa, kortti1.arvo)
+print("2. kortti on:", kortti2.maa, kortti2.arvo)
+print("3. kortti on:", kortti3.maa, kortti3.arvo)
+print("4. kortti on:", kortti4.maa, kortti4.arvo)
+print("5. kortti on:", kortti5.maa, kortti5.arvo)
