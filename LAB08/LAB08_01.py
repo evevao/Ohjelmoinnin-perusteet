fe856b513418f46b8a1c24c371079696265e91f0
Viola Ollila <AD7939@student.jamk.com>
2023-03-28 10:29:37 +0300

    LAB08_01

--- LAB08/LAB08_01.py
+++ LAB08/LAB08_01.py
@@ -1,1 +1,19 @@
 #LAB08 tehtävä 01
+
+kaverit = 10
+nimet = []
+kaikki = ""
+
+
+for i in range(kaverit):
+    nimi = input("Anna kaverisi nimi: ")
+    nimet = nimet + [nimi]
+    kaikki = ", ".join(nimet)
+
+nimi = input("Anna kaverisi nimi: ")
+nimet = nimet + [nimi]
+nimet.reverse()
+kaikki_toisinpain = ", ".join(nimet)
+
+print(kaikki)
+print(kaikki_toisinpain)
