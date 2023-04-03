4de697265ebe20368422a96e7f6b6077a2f18921
Viola Ollila <AD7939@student.jamk.com>
2023-04-03 18:43:59 +0300

    korjaus

--- LAB09/LAB09_04.py
+++ LAB09/LAB09_04.py
@@ -1,15 +1,15 @@
 #LAB09 tehtävä 04
 
 nimilista = {}
-lukumäärä = 0
+key = 0
 
 with open("nimet.txt", "r") as tiedosto:
     for nimi in tiedosto:
-        nimilista[lukumäärä] = nimi
+        nimilista[key] = nimi
         value = nimi.split()
-        lukumäärä = lukumäärä + 1
+        key = key + 1
 
-print("Nimiä listassa on:", lukumäärä)
+print("Nimiä listassa on:", key)
 
 kuinkaMonta = {value: list(nimilista.values()).count(value) for value in nimilista.values()}
 print(kuinkaMonta)
