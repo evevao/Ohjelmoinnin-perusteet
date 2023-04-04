e518832f32044307bfd5128e8a10dd1bc85c1b6e
Viola Ollila <AD7939@student.jamk.com>
2023-04-04 17:34:12 +0300

    LAB10_04

--- LAB10/LAB10_04.py
+++ LAB10/LAB10_04.py
@@ -1,2 +1,18 @@
 #LAB10 tehtävä 04
 
+nameslist = {}
+key = 0
+
+with open("names.txt", "r") as tiedosto:
+    for name in tiedosto:
+        nameslist[key] = name
+        value = name.split()
+        key = key + 1
+    print("Tiedosto names.txt luettu.")
+    longestName = ""
+    for value in nameslist.values():
+        if len(value) > len(longestName):
+            longestName = value
+            
+print("Listassa on", key, "nimeä.")
+print("Pisin nimi on:", longestName)
